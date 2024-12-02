78adffbca2351d2ef5946bd74f26e5de82f5b464
Sean Yang <seanxxyang@gmail.com>
2024-12-01 20:25:36 -0500

    Update day1part2.cpp

--- day1part2.cpp
+++ day1part2.cpp
@@ -5,25 +5,25 @@
 using namespace std;
 
 int main() {
-	vector<int> l1;
-	vector<int> l2;
-	l1.resize(1000);
-	l2.resize(1000);
+    vector<int> l1;
+    vector<int> l2;
+    l1.resize(1000);
+    l2.resize(1000);
 
-	unordered_map<int, int> freq;
+    unordered_map<int, int> freq;
 
-	for (uint i=0; i<1000; i++) {
-		cin >> l1[i];
-		cin >> l2[i];
+    for (uint i=0; i<1000; i++) {
+        cin >> l1[i];
+        cin >> l2[i];
 
-		freq[l2[i]] += 1;
-	}
+        freq[l2[i]] += 1;
+    }
 
-	int sim = 0;
+    int sim = 0;
 
-	for (uint i=0; i<1000; i++) {
-		sim += freq[l1[i]] * l1[i];
-	}
+    for (uint i=0; i<1000; i++) {
+        sim += freq[l1[i]] * l1[i];
+    }
 
-	cout << sim;
+    cout << sim;
 }
