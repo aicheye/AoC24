0e1ca049b44ae73580237285f2354ba7c761c96c
Sean Yang <seanxxyang@gmail.com>
2024-12-01 20:03:09 -0500

    Create day1part1.cpp

---
+++ day1part1.cpp
@@ -0,0 +1,27 @@
+// Source: https://usaco.guide/general/io
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	vector<int> l1;
+	vector<int> l2;
+	l1.resize(1000);
+	l2.resize(1000);
+
+	for (uint i=0; i<1000; i++) {
+		cin >> l1[i];
+		cin >> l2[i];
+	}
+
+	sort(l1.begin(), l1.end());
+	sort(l2.begin(), l2.end());
+
+	int ans = 0;
+
+	for (uint i=0; i<1000; i++) {
+		ans += abs(l1[i] - l2[i]);
+	}
+
+	cout << ans;
+}
