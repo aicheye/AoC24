8475893f42b4ecc61d831af606950cbb3d83c86c
Sean Yang <seanxxyang@gmail.com>
2024-12-01 20:25:14 -0500

    Update day1part1.cpp

--- day1part1.cpp
+++ day1part1.cpp
@@ -4,24 +4,24 @@
 using namespace std;
 
 int main() {
-	vector<int> l1;
-	vector<int> l2;
-	l1.resize(1000);
-	l2.resize(1000);
+    vector<int> l1;
+    vector<int> l2;
+    l1.resize(1000);
+    l2.resize(1000);
 
-	for (uint i=0; i<1000; i++) {
-		cin >> l1[i];
-		cin >> l2[i];
-	}
+    for (uint i=0; i<1000; i++) {
+        cin >> l1[i];
+        cin >> l2[i];
+    }
 
-	sort(l1.begin(), l1.end());
-	sort(l2.begin(), l2.end());
+    sort(l1.begin(), l1.end());
+    sort(l2.begin(), l2.end());
 
-	int ans = 0;
+    int ans = 0;
 
-	for (uint i=0; i<1000; i++) {
-		ans += abs(l1[i] - l2[i]);
-	}
+    for (uint i=0; i<1000; i++) {
+        ans += abs(l1[i] - l2[i]);
+    }
 
-	cout << ans;
+    cout << ans;
 }
